42716b51fb71704fb0ba2130120b7fde69083b12
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2017-05-27 23:47:44 +0100

    two scrape functions for the price of fun

--- l1.py
+++ l1.py
@@ -1,10 +1,52 @@
 #!/usr/bin/python3
 
 import requests
+from lxml import html
+
+
+def i_scraper(url):
+    try:
+        page = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        return 0
+    
+    tree = html.fromstring(page.content)
+    
+    results = tree.xpath('//td[@class="llistrowstyle"]/a/@href')
+    
+    return results 
+
 
 def scraper(url):
-    results = [None] * 10
-    results[0] = url 
+    field_names = ['url', 'first_name', 'last_name', 'email', 
+                   'company_name', 'phone_number', 'fax_number', 'address1', 
+                   'address2', 'city', 'zip', 'state', 'website', 'category', 
+                   'description', 'facebook', 'twitter', 'pinterest', 
+                   'linkedin']
+    
+    xpath_q = {'first_name':'//doink',
+               'last_name':'//doink',
+               'email':'//doink',
+               'company_name':'//h1/text()',
+               'phone_number':'//doink',
+               'fax_number':'//doink' ,
+               'address1':'//td[div="Address:"]/../td[2]/div/b/text()',
+               'address2':'//doink' ,
+               'city':'//td[contains(text(), "City")]/../td[2]/b[1]/text()',
+               'state':'//td[contains(text(), "City")]/../td[2]/b[2]/text()' ,
+               'zip':'//td[contains(text(), "City")]/../td[2]/b[3]/text()',
+               'state':'//td[contains(text(), "City")]/../td[2]/b[2]/text()' ,
+               'website':'//td[div="WebSite:"]/../td[2]/b/text()',
+               'category':'//doink' ,
+               'description':'//doink' ,
+               'facebook':'//doink' ,
+               'twitter':'//doink' ,
+               'pinterest':'//doink' ,
+               'linkedin':'//doink' }
+    
+    results = {}
+    results['url'] = url 
+    
     try:
         page = requests.get(url)
     except requests.exceptions.ConnectionError:
@@ -12,6 +54,10 @@ def scraper(url):
         
     tree = html.fromstring(page.content)
     
-    
-    
+    for item in xpath_q.keys():
+        try:
+            results[item] = tree.xpath(xpath_q[item])[0]
+        except IndexError:
+            results[item] = None
+            
     return(results)
--- t.py
+++ t.py
@@ -2,7 +2,25 @@
 
 import sys
 import l1
+import csv
 
-url = sys.argv[2]
+url = sys.argv[1]
 
-print(l1.scraper(url))
+url_list = l1.i_scraper(url)
+
+print("Length ", len(url_list))
+
+with open('test.csv', 'w') as csvfile:
+    wr = csv.DictWriter(csvfile, 
+                        fieldnames = ['url', 'first_name', 'last_name', 'email',
+                                      'company_name', 'phone_number', 
+                                      'fax_number', 'address1', 'address2',
+                                      'city', 'zip', 'state', 'website',
+                                      'category', 'description', 'facebook',
+                                      'twitter', 'pinterest', 'linkedin'])
+    wr.writeheader()
+    counter = 1
+    for url in url_list:
+        wr.writerow(l1.scraper('http://www.greenpeople.org' + url))
+        print(counter)
+        counter = counter + 1 
